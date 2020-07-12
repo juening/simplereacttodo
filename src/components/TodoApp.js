e34e0a136221a46d0e42fec9a6df3b262197dcb8
Jue Ning <juening@gmail.com>
2020-07-11 22:36:46 -0400

    styling the app

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import NewTodoForm from './NewTodoForm';
 import TodoList from './TodoList';
 
-class TodoApp extends Component {
-  render() {
-    return (
-      <div>
-        <NewTodoForm />
-        <TodoList />
-      </div>
-    );
-  }
-}
-export default TodoApp;
+import styles from '../styles/TodoAppStyles';
+
+const TodoApp = ({ classes }) => {
+  return (
+    <Paper className={classes.root} elevation={0}>
+      <Grid container justify="center">
+        <Grid item xs={11} md={8} lg={6}>
+          <AppBar position="static" color="primary" style={{ height: '64px' }}>
+            <Toolbar>
+              <Typography>Todo App with React</Typography>
+            </Toolbar>
+          </AppBar>
+          <NewTodoForm />
+          <TodoList />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default withStyles(styles)(TodoApp);
---
+++ src/styles/TodoAppStyles.js
@@ -0,0 +1,8 @@
+export default {
+  root: {
+    padding: 0,
+    margin: 0,
+    height: '100vh',
+    backgroundColor: '#fafafa',
+  },
+};
