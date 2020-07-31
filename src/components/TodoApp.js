05834b6bdfa26696679be40392a49af4fa41d21a
Jue Ning <juening@gmail.com>
2020-07-30 22:15:46 -0400

    implemented setCurrentUser in TodoApp

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { auth } from '../firebase/firebase';
+import { setCurrentUser } from '../redux/user/userActions';
 
 import Home from './Home';
 import SignIn from './SignIn';
 
 class TodoApp extends Component {
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      console.log(userAuth);
+
+      setCurrentUser(userAuth);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render() {
     return (
       <Switch>
@@ -15,4 +33,12 @@ class TodoApp extends Component {
   }
 }
 
-export default TodoApp;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoApp);
--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,9 +1,20 @@
+import { SET_CURRENT_USER } from './userTypes';
+
 const initialState = {
   currentUser: null,
 };
 
 const userReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default userReducer;
